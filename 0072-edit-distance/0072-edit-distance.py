162b9e0a511e6bdd69a31efc401b949f5f0e5382
Anurag Chaudhary <104416643+Anurag1Chaudhary@users.noreply.github.com>
2024-04-25 14:47:18 +0530

    Time: 51 ms (98.77%), Space: 14.2 MB (90.04%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        dp=[[-1]*len(word2) for _ in range(len(word1))]
+        def imao(i,j):
+            if i==-1:
+                return j+1;
+            elif j==-1:
+                return i+1;
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            elif word1[i]==word2[j]:
+                dp[i][j]=imao(i-1,j-1)
+            else:
+                dp[i][j]=1+min(imao(i-1,j-1),imao(i,j-1),imao(i-1,j))
+            return dp[i][j]
+        return imao(len(word1)-1,len(word2)-1)
