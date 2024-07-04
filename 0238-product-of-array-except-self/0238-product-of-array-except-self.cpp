27043fc1975733d6535c75b8e045668730cf12ee
Anurag Chaudhary <104416643+Anurag1Chaudhary@users.noreply.github.com>
2024-07-04 14:40:37 +0530

    Time: 33 ms (21.84%), Space: 40 MB (38.34%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int size = nums.size();
-        vector<int> suffixArray(size);
-        
-        // Calculate suffix products
-        for(int indx = size - 1; indx > -1; indx--) {
-            if(indx == size - 1)
-                suffixArray[indx] = nums[indx];
-            else
-                suffixArray[indx] = nums[indx] * suffixArray[indx + 1];
+        int n=nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        vector<int> ans(n);
+        left[0]=1;
+        right[n-1]=1;
+        for(int i=1;i<n;i++)
+        {
+            left[i]=left[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>-1;i--)
+        {
+            right[i]=right[i+1]*nums[i+1];
         }
         
-        // Calculate prefix products and combine them with suffix products
-        int prefixProduct = 1;
-        for(int indx = 0; indx < size; indx++) {
-         //ternary operator  
-            int suffixProduct = (indx + 1 < size) ? suffixArray[indx + 1] : 1;
-            suffixArray[indx] = prefixProduct * suffixProduct;
-            prefixProduct *= nums[indx];
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=left[i]*right[i];
         }
         
-        return suffixArray;
+        return ans;
     }
 };
